cb12df54629f9bab5bfc8056f4482739845312f9
Saniya-Javed <saniya.cse.rymec@gmail.com>
2025-04-07 20:47:56 +0530

    Delete job recommendation system directory

--- job recommendation system/admin.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-con = sqlite3.connect('job_recommend.db')
-cr = con.cursor()
-
-cr.execute("create table if not exists admin(username TEXT, password TEXT)")
-
-cr.execute("insert into admin values('admin', 'admin')")
-con.commit()
